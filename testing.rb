842acce480451e0b7c08be4cf7e06c844ad6c4d7
wbosw <146214919+wbosw@users.noreply.github.com>
2023-10-14 04:48:33 +0000

    think fast finished

---
+++ testing.rb
@@ -0,0 +1,7 @@
+x = false
+
+if x # Replace this with each expression.
+  pp "The expression is truthy."
+else
+  pp "The expression is falsy."
+end
--- thinking_fast.rb
+++ thinking_fast.rb
@@ -12,12 +12,30 @@ unpredictable_inputs = [
 some_random_input = unpredictable_inputs.sample
 # write your program below
 
-pp some_random_input = Time.now
+#pp thing = [5, 2]
+
+#pp thing.at(2)
+
+pp some_random_input = :GOODBYE 
 
 if some_random_input.class == String
   pp some_random_input.downcase
 elsif some_random_input.class == Time
   pp some_random_input.strftime("%A").downcase
-  elsif
-    some_random_input   
+elsif
+    some_random_input.class == Integer
+    if some_random_input.even?
+      pp some_random_input.to_s + " is even"
+    else pp some_random_input.to_s + " is odd"
+    end
+elsif some_random_input.class == Symbol
+  pp some_random_input.downcase
+elsif some_random_input.class == NilClass
+  pp "no object provided"
+elsif some_random_input.class == Hash
+  pp some_random_input.keys
+elsif some_random_input 
+  pp "you may pass"
+elsif some_random_input != true
+  pp "you may not pass"
 end
